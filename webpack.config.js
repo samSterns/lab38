ffb9b0b26cbbec4f263d06572b9165a8c2c26aea
samSterns <sam.g.sterns@gmail.com>
2020-02-12 13:48:15 -0800

    all test pass now with starter code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const HtmlPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const CopyPlugin = require('copy-webpack-plugin');
 
 // eslint-disable-next-line
 module.exports = {
@@ -16,13 +14,7 @@ module.exports = {
   },
   plugins: [
     new HtmlPlugin({ template: './src/index.html' }),
-    new CleanWebpackPlugin(),
-    new Dotenv({
-      systemvars: true
-    }),
-    new CopyPlugin([
-      { from: 'public' },
-    ])
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
